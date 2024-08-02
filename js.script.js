7d46b9875029a64d23cce70089f4af83aee1bf7e
Luellen Rayol <114498976+luellenrayol@users.noreply.github.com>
2024-08-02 15:06:43 -0300

    Add files via upload
    teste de variáveis

---
+++ js.script.js
@@ -0,0 +1,5 @@
+function ArmazenarValor(){
+var userInput = document.getElementById("userInput").value;
+console.log("valor da variavel é:" + userInput);
+document.getElementById("valorInserido").innertext = "o valor inserido foi" + userInput;
+}
